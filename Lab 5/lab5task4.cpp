4401add26c1047bfc2d6823da81e6968aa9347d8
HamzaF69 <136762872+HamzaF69@users.noreply.github.com>
2025-09-28 22:42:55 +0500

    Update lab5task4.cpp

--- Lab 5/lab5task4.cpp
+++ Lab 5/lab5task4.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int ackermann(int m,int n){
-    if (m==0)
-    {
+    if (m==0){
         return n+1;
     }else if(n==0 && m>0){
         return ackermann(m-1,1);
@@ -18,4 +17,5 @@ int main() {
     cin>>m>>n;
     cout<<"ackerman: "<<ackermann(m,n);
     return 0;
+
 }
